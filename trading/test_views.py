5d25c97d93b1db505e69afbcab90910c429b9125
Weber <andweber92@gmail.com>
2023-09-15 09:40:59 -0300

    test: insert unit test for view

---
+++ trading/test_views.py
@@ -0,0 +1,16 @@
+import pytest
+from .models import Asset
+from .views import send_email_alert
+
+
+@pytest.mark.django_db
+def test_send_email_alert(mocker):
+    mocker.patch("trading.views.send_mail")
+    asset = Asset.objects.create(
+        ticker="ABC",
+        lower_limit=10.0,
+        upper_limit=20.0,
+        check_interval=30,
+        email="test@example.com",
+    )
+    send_email_alert(asset, "Buy")
