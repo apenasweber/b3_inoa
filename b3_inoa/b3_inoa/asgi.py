8096ae1ac5eeda33160aa2c998952da53070d2f1
Weber <andweber92@gmail.com>
2023-09-15 09:33:25 -0300

    Initial Structure

---
+++ b3_inoa/b3_inoa/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for b3_inoa project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "b3_inoa.settings")
+
+application = get_asgi_application()
